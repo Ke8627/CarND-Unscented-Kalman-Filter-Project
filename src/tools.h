efe7a06d0999e34db1defde431448ed7d26494cf
Jeffrey Liu <writetojeffrey@gmail.com>
2017-06-04 23:21:33 -0700

    Change Tools to namespace.

--- src/tools.h
+++ src/tools.h
@@ -7,23 +7,13 @@ using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using namespace std;
 
-class Tools {
-public:
-  /**
-  * Constructor.
-  */
-  Tools();
-
-  /**
-  * Destructor.
-  */
-  virtual ~Tools();
-
+namespace Tools
+{
   /**
   * A helper method to calculate RMSE.
   */
   VectorXd CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
 
-};
+}
 
 #endif /* TOOLS_H_ */
